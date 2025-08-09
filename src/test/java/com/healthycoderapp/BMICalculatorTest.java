f3cde2a658e083dc850002795dba01cbf1de0035
Henrique Breviglieri Suguihura <hbs@ibm.com>
2025-08-09 11:06:37 -0300

    adding tests

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -2,9 +2,25 @@ package com.healthycoderapp;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 class BMICalculatorTest {
+	
+	@BeforeAll //setting up db connections or servers, etc. (operations that are expensive to run before every test)
+	static void beforeAll() {
+		System.out.println("Before all unit tests.");
+	}
+	
+	@AfterAll // close db connections, stop servers, etc.
+	static void afterAll() {
+		System.out.println("After all unit tests.");
+	}
 
 	@Test
 	void shouldReturnTrueWhenDietRecommended() {
@@ -30,6 +46,65 @@ class BMICalculatorTest {
 		
 		// Then
 		assertFalse(recommended);
+	}
+	
+	@Test
+	void shouldThrowArithmeticExceptionWhenHeightZero() {
+		// Given
+		double weight = 50.0;
+		double height = 0.0;
+		
+		// When
+		Executable executable = () -> BMICalculator.isDietRecommended(weight, height);
+		
+		// Then
+		assertThrows(ArithmeticException.class, executable);
+	}
+	
+	@Test
+	void shouldReturnCoderWithWorstBMIWhenCoderListNotEmpty() {
+		// Given
+		List<Coder> coders = new ArrayList<>();
+		coders.add(new Coder(1.80, 60.0));
+		coders.add(new Coder(1.82, 98.0));
+		coders.add(new Coder(1.82, 64.7));
+		
+		// When
+		Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+		
+		// Then
+		assertAll (
+			() -> assertEquals(1.82, coderWorstBMI.getHeight()),
+			() -> assertEquals(98.0, coderWorstBMI.getWeight())
+		);
+	}
+	
+	@Test
+	void shouldReturnNullWorstBMICoderWhenCoderListIsEmpty() {
+		// Given
+		List<Coder> coders = new ArrayList<>();
+		
+		// When
+		Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+		
+		// Then
+		assertNull(coderWorstBMI);
+	}
+	
+	@Test
+	void shouldReturnCorrectBMIScoreArrayWhenCoderListNotEmpty() {
+		// Given
+		List<Coder> coders = new ArrayList<>();
+		coders.add(new Coder(1.80, 60.0));
+		coders.add(new Coder(1.82, 98.0));
+		coders.add(new Coder(1.82, 64.7));
+		double[] expected = {18.52, 29.59, 19.53};
+		
+		// When
+		double[] bmiScores = BMICalculator.getBMIScores(coders);
+		
+		// Then
+		assertArrayEquals(expected, bmiScores); // we need this method for arrays because of memory address
 	}
 
 }
